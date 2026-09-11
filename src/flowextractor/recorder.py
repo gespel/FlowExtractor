fefbf077ef4c89f9552154bebf2b5126c0f9c0b5
gespel <sten.heimbrodt@hotmail.de>
2026-09-11 11:17:40 +0200

    save output parameter

--- src/flowextractor/recorder.py
+++ src/flowextractor/recorder.py
@@ -21,30 +21,34 @@ class PacketRecorder:
         else:
             logger.info("No packets to save.")
 
-    def record(self, length: int):
+    def record(self, length: int, save_output: bool):
         if length == None:
             logger.info("Recording packets indefinitely...")
             while True:
                 self.packets = sniff(timeout=3)
                 timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-                self.save_packets_to_file(f"recorded_packets_{timestamp}.pcap")
+                if save_output:
+                    self.save_packets_to_file(f"recorded_packets_{timestamp}.pcap")
         else:
             self.packets = sniff(timeout=length)
             timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-            self.save_packets_to_file(f"recorded_packets_{timestamp}.pcap")
+            if save_output:
+                self.save_packets_to_file(f"recorded_packets_{timestamp}.pcap")
 
 
 def main():
     if os.geteuid() != 0:
         logger.info("This script requires root privileges. Re-running with sudo...")
+        subprocess.run(["uv", "sync"])
         subprocess.run(["sudo", ".venv/bin/python3", "-m", "src.flowextractor.recorder"] + sys.argv[1:])
         return
     arg_parser = argparse.ArgumentParser(description="Record packets for a specified duration.")
     arg_parser.add_argument("--record_length", type=int, help="Length of time to record packets in seconds")
+    arg_parser.add_argument("--save_output", type=bool, help="Whether to save the recorded packets to a file")
     args = arg_parser.parse_args()
 
     recorder = PacketRecorder()
-    recorder.record(length=args.record_length)
+    recorder.record(length=args.record_length, save_output=args.save_output)
 
 if __name__ == "__main__":
     main()
