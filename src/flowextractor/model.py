a654a735e186ff31efa8dfe6903b854615db9d50
gespel <sten.heimbrodt@hotmail.de>
2026-09-14 16:15:48 +0200

    this works

--- src/flowextractor/model.py
+++ src/flowextractor/model.py
@@ -23,7 +23,7 @@ class AttackDetectionNet(torch.nn.Module):
     def forward(self, x):
         return self.layers(x)
 
-    def train(self, training_data, epochs=100, learning_rate=0.001):
+    def train(self, training_data, epochs=25, learning_rate=0.001):
         optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
         loss_fn = torch.nn.BCEWithLogitsLoss()
         for epoch in range(epochs):
@@ -64,10 +64,21 @@ for i in range(len(x_df)):
 print(f"{x_df.info()}\n")
 print(y_df.head())
 
-training_data = []
-for i in range(len(x)):
-    training_data.append((torch.tensor(x[i], dtype=torch.float32), torch.tensor(y[i], dtype=torch.float32)))
+X_train, X_test = x[:int(len(x)*0.8)], x[int(len(x)*0.8):]
+y_train, y_test = y[:int(len(y)*0.8)], y[int(len(y)*0.8):]
 
 m = AttackDetectionNet()
 print(m)
-m.train(training_data)
+m.train(list(zip([torch.tensor(xi, dtype=torch.float32) for xi in X_train], [torch.tensor(yi, dtype=torch.float32) for yi in y_train])))
+validation_data = list(zip([torch.tensor(xi, dtype=torch.float32) for xi in X_test], [torch.tensor(yi, dtype=torch.float32) for yi in y_test]))
+print(f"Validation data prepared with {len(validation_data)} samples.")
+results = [(x, y, m.forward(x)) for x, y in validation_data]
+for x, y, y_pred in results:
+    print(f"True Label: {y} Predicted: {torch.sigmoid(y_pred)}")
+TP = [1 if y == 1 and torch.sigmoid(y_pred) >= 0.7 else 0 for x, y, y_pred in results]
+FP = [1 if y == 0 and torch.sigmoid(y_pred) >= 0.7 else 0 for x, y, y_pred in results]
+TN = [1 if y == 0 and torch.sigmoid(y_pred) < 0.7 else 0 for x, y, y_pred in results]
+FN = [1 if y == 1 and torch.sigmoid(y_pred) < 0.7 else 0 for x, y, y_pred in results]
+
+print(f"TP: {sum(TP)}, FP: {sum(FP)}, TN: {sum(TN)}, FN: {sum(FN)}")
+
